34761a71bf12182c26d4ef581d43c4f4afcf7249
Arun Pratap Singh <your.sarunpratap049@gmail.com>
2025-09-09 16:26:53 +0530

    Update metaToken in config.js to the latest value

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-  metaToken: "EAAJkBk85o2gBPZAeeEmr2RQZBxVT4ZAH9EDRMVZBGjHSxps0WwMkRqp27igL3QNZCYnhyuwzwJoUEspkMllW1ZCQ6wrw6MXM5KHykBJ7Euma1BrgjEJ3nW0tkSPkyzfqJve9zZAQNNZCCmtfSo6HNBTXXNnXbLg254oPokQE7gJo2zBWuoc0gBEqmbXWOlRm7LmysXhns6jZC6kwf0rUzvfvURcfgWLkZB1HgjeD7MuUrPLQZDZD",
+  metaToken: "EAAJkBk85o2gBPS7qMIuyD0EovUk9ZAa4zdcpgJdanJAyPTZCZAlGa0Ugoi8PirNrCHEzI8GTVlxaMDprA5qVYFCVuoJfhosQednYoIOZArdEZBrvehgvzCkbwn3KyzMG21oLVxk8fkoj7Kzh8ZAJkVNdDFFO4asS99wjuZCpeJfXQE7BTrcUpBIDyZBxyNeHRV8Tqo0p4lm0PbZCGvM0NLQqKZAr6Pek1u2fZA0ZApxywVpnCwZDZD",
   phoneNumberId: "754571211077181",
   webhookVerifyToken: "myVerify123",
   port: 3000,
