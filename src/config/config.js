58ead90166c298d1fdf9e370db256e2120195a38
Arun Pratap Singh <your.sarunpratap049@gmail.com>
2025-09-09 10:39:24 +0530

    change th config file

--- src/config/config.js
+++ src/config/config.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-  metaToken: process.env.META_TOKEN,
-  phoneNumberId: process.env.PHONE_NUMBER_ID,
-  webhookVerifyToken: process.env.WEBHOOK_VERIFY_TOKEN || 'myVerify123',
-  port: process.env.PORT || 3000,
+  metaToken: "EAAJkBk85o2gBPfXTNe4L50M9iHgZAXl8YUYvsQ4iy8M3ixZCU2QJs0ih402KZCAud0tZABn82X6iebSBXmpDIZAHPjL5utLd82XsnhcLUF7SlcYvdQQyRAmDK2rwuA5yIcfZCZCeuZAJP2mbA4psJz7MTJZB91oaqbZAJGBudWlZC6VZCXtlyoDbqJtYzi6px8OEQ9TjErMgPZBwikYk9HbQ0ZAwLbW5dskVsjlS5TANAI1sFq3AZDZD",
+  phoneNumberId: "754571211077181",
+  webhookVerifyToken: "myVerify123",
+  port: 3000,
 };
