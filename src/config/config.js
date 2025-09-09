0371659fa75cb253564c0c79797a4cf2f028e9f3
Arun Pratap Singh <your.sarunpratap049@gmail.com>
2025-09-09 12:45:34 +0530

    change the meta token

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-  metaToken: "EAAJkBk85o2gBPZAC1oVlBDB21KOrdUVZAwQ0NedkRibkSekyDc9ZA8IkRBGYHLPlhxrd1FjzuZCJZCRIuqfhZCt0IzzhJHOEdjGhPLIveXmldDDeq4ZCL1RsZArcxtCojjh81Y4Of2JRcFJjjljXnZC0HjJCJJWH5xxccWZC0SRKs4BY0CteloYxm6ZCd46xA99OeNBI046Yvtx1xdwkFNCOMLTQU3bT9WiyiA2ih1J3EybgMjb",
+  metaToken: "EAAJkBk85o2gBPfMVrauH5ZCOxhztLZAha7ZBNOSdkAT0iGlLURlIMUxyJ5ACIKvgGst6QQmrAqbX78ufVNZCuGC9cr9bj83HXkqw38IBspecZCsUUovb3xQTRPIHnnr9yYzZAsFSDJL1NG3FHjFsM09NrUmULFNUNTNhgyCfCgeIiYIWVDoey5yrjrZBvQgtMNPnMcTHaQTwiPdSsH62nGuNs3taxGqgFIQi8NdXZBqc7AZDZD",
   phoneNumberId: "754571211077181",
   webhookVerifyToken: "myVerify123",
   port: 3000,
