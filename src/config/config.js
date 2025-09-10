f8896e26fdb223880cca4ef614b67cca3244303d
Arun Pratap Singh <your.sarunpratap049@gmail.com>
2025-09-10 12:01:01 +0530

    Refactor index.js to integrate webhook routes and update server configuration in config.js

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-  metaToken: "EAAJkBk85o2gBPacXDsqlO9PPZBzYKh1G3aHWGZApmHMZAODJnLA5c9wHQ0CatrYKsKgwi8UAW23UIZBfVcNnLXkx6wN0sfD9prgvvqDWBSZBcKVUbaxjzqaBXcRxC8g1CMdbOn39fVPfyLijcT6ofqNXn4tZAKRkUQcnouduTRSrXcSGcn1xDGuveQzLhwgyyKUyq481txQdvmBSH6jgRpi9HZCYcYUltEQT3bDT7AfCQZDZD",
+  metaToken: "EAAJkBk85o2gBPQoOnp6DS1ZAlyOPZC8XYQykOfzwM36iVVAolDbVWu88CqPyrZCZAgPG4LWze83ziGOTwqZBvIdBKy2Nets8Xc9nFn2LGbDW5LWXzv1kVWZCZAPlT8FYtnOQiko506TkYQjJxxw5NgCtGaKRFbxEbvTVEjPIKQ7xj6OT8eex8m84UHuZBoVbkxZA4PbbU0uLGfTOKqKDxHDidKSHbl9h9IOnDyiVYgDjMDAZDZD",
   phoneNumberId: "754571211077181",
   webhookVerifyToken: "myVerify123",
   port: 3000,
